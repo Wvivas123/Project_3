ca2a25ee4987b53b37deb4260ec613dca8598b6d
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-30 21:35:08 -0400

    established mongoose connection to mongodb

--- project_3/server.js
+++ project_3/server.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+
+// Connecting to MongoDB
+// mongoose.connect("mongodb://localhost:3000");
+const MONGODB_URI =
+process.env.MONGODB_URI || "mongodb://localhost:3000";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
  
-app.listen(3000)
+app.listen(3000);
 
-console.log("Server is running!")
+console.log("Server is running!");
